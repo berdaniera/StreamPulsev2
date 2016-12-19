f82480d22d37ca9f426d67e6a22d38134de54825
Aaron Berdanier <aaron.berdanier@gmail.com>
2016-12-19 11:49:46 -0500

    update hobo upload

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def allowed_file(filename):
 
 def read_hobo(f):
     xt = pd.read_csv(f, skiprows=[0])
-    cols = [x for x in xt.columns.tolist() if re.match("^#$|Coupler|File|Host|Connected|Attached|Stopped|End",x) is None]
+    cols = [x for x in xt.columns.tolist() if re.match("^#$|Coupler|File|Host|Connected|Attached|Stopped|End|Unnamed",x) is None]
     xt = xt[cols]
     m = [re.sub(" ","",x.split(",")[0]) for x in xt.columns.tolist()]
     u = [x.split(",")[1].split(" ")[1] for x in xt.columns.tolist()]
