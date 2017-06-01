ac64cc077ed9d94724babdc1a919348492e4265d
Aaron Berdanier <aaron.berdanier@gmail.com>
2017-06-01 15:19:09 -0400

    update archiving

--- automated_sb_archiving.py
+++ automated_sb_archiving.py
@@ -12,7 +12,7 @@ time.sleep(5)
 # metadata
 metaf = os.listdir(cfg.META_FOLDER) # metadata files
 insb = sb.get_item(cfg.SB_META) # get files
-fin_sb = [f['name'] for f in insb['files'] if len(insb)>0] # get file names
+fin_sb = [f['name'] for f in insb['files'] if 'files' in insb] # get file names
 updata = [cfg.META_FOLDER+"/"+x for x in metaf if x not in fin_sb]
 metares = sb.upload_files_and_update_item(insb, upmeta)
 
@@ -21,6 +21,6 @@ time.sleep(2)
 # original data
 dataf = os.listdir(cfg.UPLOAD_FOLDER) # metadata files
 insb = sb.get_item(cfg.SB_DATA) # get files
-fin_sb = [f['name'] for f in insb['files'] if len(insb)>0] # get file names
+fin_sb = [f['name'] for f in insb['files'] if 'files' in insb] # get file names
 updata = [cfg.UPLOAD_FOLDER+"/"+x for x in dataf if x not in fin_sb]
 datares = sb.upload_files_and_update_item(insb, updata)
