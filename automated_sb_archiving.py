cf907eb46002285fe7d130ce479e3543cb9aae06
Aaron Berdanier <aaron.berdanier@gmail.com>
2017-06-01 15:11:17 -0400

    add automated archiving

---
+++ automated_sb_archiving.py
@@ -0,0 +1,25 @@
+import pysb
+import config as cfg
+import os
+import time
+
+## login to sb
+sb = pysb.SbSession()
+sb.login(cfg.SB_USER, cfg.SB_PASS)
+time.sleep(5)
+
+## check uploaded files against sb files
+# metadata
+metaf = os.listdir(cfg.META_FOLDER) # metadata files
+insb = sb.get_item(cfg.SB_META) # get files
+fin_sb = [f['name'] for f in insb['files']] # get file names
+updata = [cfg.META_FOLDER+"/"+x for x in metaf if x not in fin_sb]
+metares = sb.upload_files_and_update_item(insb, upmeta)
+time.sleep(5)
+
+# original data
+dataf = os.listdir(cfg.UPLOAD_FOLDER) # metadata files
+insb = sb.get_item(cfg.SB_DATA) # get files
+fin_sb = [f['name'] for f in insb['files']] # get file names
+updata = [cfg.UPLOAD_FOLDER+"/"+x for x in dataf if x not in fin_sb]
+datares = sb.upload_files_and_update_item(insb, updata)
