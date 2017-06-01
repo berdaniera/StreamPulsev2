f20e82a92900a9f52ad40b00a684678db2bbf855
Aaron Berdanier <aaron.berdanier@gmail.com>
2017-06-01 15:17:10 -0400

    update archiving

--- automated_sb_archiving.py
+++ automated_sb_archiving.py
@@ -12,17 +12,15 @@ time.sleep(5)
 # metadata
 metaf = os.listdir(cfg.META_FOLDER) # metadata files
 insb = sb.get_item(cfg.SB_META) # get files
-if len(insb)>0:
-    fin_sb = [f['name'] for f in insb['files']] # get file names
-    updata = [cfg.META_FOLDER+"/"+x for x in metaf if x not in fin_sb]
-    metares = sb.upload_files_and_update_item(insb, upmeta)
+fin_sb = [f['name'] for f in insb['files'] if len(insb)>0] # get file names
+updata = [cfg.META_FOLDER+"/"+x for x in metaf if x not in fin_sb]
+metares = sb.upload_files_and_update_item(insb, upmeta)
 
 time.sleep(2)
 
 # original data
 dataf = os.listdir(cfg.UPLOAD_FOLDER) # metadata files
 insb = sb.get_item(cfg.SB_DATA) # get files
-if len(insb)>0:
-    fin_sb = [f['name'] for f in insb['files']] # get file names
-    updata = [cfg.UPLOAD_FOLDER+"/"+x for x in dataf if x not in fin_sb]
-    datares = sb.upload_files_and_update_item(insb, updata)
+fin_sb = [f['name'] for f in insb['files'] if len(insb)>0] # get file names
+updata = [cfg.UPLOAD_FOLDER+"/"+x for x in dataf if x not in fin_sb]
+datares = sb.upload_files_and_update_item(insb, updata)
